dfbb12d35189f2304493489ca9bc1786d39fbb22
Alessandro220 <a.losappio2@studenti.poliba.it>
2026-09-12 17:07:37 +0200

    Migliorie 2.1

--- frontend/src/pages/MiePrenotazioni.js
+++ frontend/src/pages/MiePrenotazioni.js
@@ -1,6 +1,5 @@
 import { useState,useContext,useEffect } from "react";
 import axios from 'axios';
-import { useNavigate } from "react-router-dom";
 import { AuthContext } from '../context/AuthContext';
 import { Card, CardContent, Typography, Button, Box } from '@mui/material';
 
